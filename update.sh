baa6f95109d9da823548ca204eaaba671259ab05
Tyler King <tyler@osiset.com>
2021-02-22 14:03:15 -0330

    Comments to update.sh

--- update.sh
+++ update.sh
@@ -10,17 +10,22 @@ result=$(find . -not -path './.git/*' -not -path './.git' -not -path './README.m
 
 for path in $result
 do
+    # Ignore "." and ignore if not an actual file
     if [ "$path" != "." ] && [ -f "$path" ]; then
         start=$(echo "$path" | cut -c3)
         if [ "$start" = "." ]; then
+            # Path starts with ".", assume its a hidden file from home directory
             fixed_path=$(echo "$path" | sed -e "s#./.#/home/$USER/.#")
         else
+            # Path is somewhere else on the system besides home
             fixed_path=$(echo "$path" | sed -e "s#./#/#")
         fi
 
+        # Show what will be copied
         echo "$fixed_path" ">" "$path"
         
         if [ "$dry_run" = 0 ]; then
+            # Do the copy if not a dry run
             cp "$fixed_path" "$path"
         fi
     fi
